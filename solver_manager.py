84e0bfdea54fde64434f5ed21d73c17d08e74821
h3llcat25 <marcial.paszkiel@googlemail.com>
2024-03-19 18:37:26 +0100

    finally connected the qubo dict generation to the solver part :)

--- solver_manager.py
+++ solver_manager.py
@@ -6,12 +6,12 @@ from param_and_solve_automation import *
 
 # ------- Main program -------
 if __name__ == "__main__":
-
-    qubo_matrix = param_search_and_auto_solver_auto_penalty_gurobipy_model_enzymes_and_matrix(
-        # "C:\\Users\\marsh\\Documents\\Python Bachelor\\QUBO_Project_BA\\graphStuff\\smallDots\\eco_filtering_dot"
-        # "\\Glycerolipid_marked.dot")
-        "C:\\Users\\marsh\\Documents\\GitHub\\BachelorQUBOProject\\graphStuff\\smallDots_w_marked_and_tests"
-        "\\eco_filtering_dot\\Polyketide.dot")
+    file_path = "/workspaces/graph-coloring/Polyketidecpo.dot"
+    qubo_matrix, graph, bin_vars_dict, marked_nodes = get_qubo_dict_for_dwave_qa(file_path)
+    # "C:\\Users\\marsh\\Documents\\Python Bachelor\\QUBO_Project_BA\\graphStuff\\smallDots\\eco_filtering_dot"
+    # "\\Glycerolipid_marked.dot")
+    # "C:\\Users\\marsh\\Documents\\GitHub\\BachelorQUBOProject\\graphStuff\\smallDots_w_marked_and_tests"
+    # "\\eco_filtering_dot\\Polyketide.dot")
 
     bqm = BinaryQuadraticModel.from_qubo(qubo_matrix)
     solver = ExactSolver()
@@ -19,11 +19,21 @@ if __name__ == "__main__":
     # Solve the BQM using the ExactSolver
     result = solver.sample(bqm)
 
-    # Print the results
-    print("Solutions:")
-    for sample, energy in result.data(['sample', 'energy']):
-        print(f"Sample: {sample}, Energy: {energy}")
-    #print(bqm)
-    #cqm = build_cqm(G, num_colors)
+    best_sample = result.first
+    print(best_sample)
 
-    #sample = run_hybrid_solver(cqm)
+    node_name_to_solulu_dict = node_to_solutionsValue_assignment_for_result(bin_vars_dict, best_sample.sample)
+
+    modify_dot_graph(graph, node_name_to_solulu_dict, file_path,
+                     marked_nodes)  # graph, solution_dictionary, dot_file, target_nodes
+    # modify_dot_graph(graph, solution_dictionary, dot_file, target_nodes)
+
+    # Print the results
+    # print("Solutions:")
+    # for sample, energy in result.data(['sample', 'energy']):
+    #     print(f"Sample: {sample}, Energy: {energy}")
+
+    # print(bqm)
+    # cqm = build_cqm(G, num_colors)
+
+    # sample = run_hybrid_solver(cqm)
