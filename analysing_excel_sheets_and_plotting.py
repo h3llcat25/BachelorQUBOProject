f3c6a327021416c7e2c5c929db235e92d5acdf1e
h3llcat25 <marcial.paszkiel@googlemail.com>
2024-04-10 20:03:57 +0200

    small changes to plot some charts for the statistics

--- analysing_excel_sheets_and_plotting.py
+++ analysing_excel_sheets_and_plotting.py
@@ -4,7 +4,7 @@ import numpy as np  # Ensure numpy is imported
 
 
 # Step 1: Read the Excel file
-file_path = 'C:\\Users\\marsh\\Documents\\GitHub\\BachelorQUBOProject\\small_dots_stats.xlsx'
+file_path = 'C:\\Users\\marsh\\Documents\\GitHub\\BachelorQUBOProject\\large_dots_stats.xlsx'
 df = pd.read_excel(file_path, usecols=range(8), nrows=23)  # Read the first 8 columns and 23 rows
 
 # Step 2: Sort the data
@@ -21,14 +21,14 @@ plt.figure(figsize=(14, 8))
 # plt.plot(x_positions, df['Nr. of total Bin Vars (Qutie)'], color='red', marker='o', linestyle='-', linewidth=2, markersize=5, label='Nr. of total Bin Vars (Qutie)')
 plt.bar(x_positions, df['Nr. of total Bin Vars (Qutie)'], color='red',label='Nr. of total Bin Vars (Qutie)', width=0.4)
 plt.bar(x_positions, df['Nr. of total Bin Vars (new Method)'], color='green', label='Nr. of total Bin Vars (new Method)', width=0.4)
-plt.bar(x_positions, df['Nr. of Nodes'], color='skyblue', tick_label=df['File Name'], label='Nr. of Nodes', width=0.4)
+plt.bar(x_positions, df['Nr. of Nodes'], color='skyblue', label='Nr. of Nodes', width=0.4)
 
 
 # Adding some chart elements for clarity
 plt.title('Comparison of Base Values and Total Values by Qutie and the new Method')
-plt.xlabel('Metabolic Networks (small)')
+plt.xlabel('Metabolic Networks (large)')
 plt.ylabel('Nr of Binary Variables')
-plt.xticks(x_positions, df['Nr. of Nodes'], rotation=45)  # Set x-ticks to be the sorted row identifiers; rotate for readability
+plt.xticks([loc - 0.5 for loc in x_positions], df['File Name'], rotation=50)  # Set x-ticks to be the sorted row identifiers; rotate for readability
 plt.legend()
 
 # Show the plot
